d620dcf3f90b06de664ec5e1964f5b6c138ffc6c
Gerson Caballero <gersoncaballero306@gmail.com>
2023-06-24 01:54:51 +0000

    resolve issue

--- erpnext/accounts/doctype/bank_book_reconciliations/bank_book_reconciliations.py
+++ erpnext/accounts/doctype/bank_book_reconciliations/bank_book_reconciliations.py
@@ -151,7 +151,6 @@ class BankBookReconciliations(Document):
 
 		self.actual_total_conciliation = self.bank_amount + self.credit_note_transit - self.bank_check_transit_amount - self.debit_note_transit + self.bank_deposit_transit + self.wire_transfer_amount
 
-		frappe.msgprint("conciliación actual {} + {} - {} - {} + {} + {}".format(self.bank_amount,self.credit_note_transit,self.bank_check_transit_amount,self.debit_note_transit,self.bank_deposit_transit,self.wire_transfer_amount))
 		self.defference_amount = self.actual_total_conciliation - self.book_balance
 
 		if self.actual_total_conciliation < 0:
@@ -222,31 +221,18 @@ class BankBookReconciliations(Document):
 
 		book_balance = self.total_last_reconciliations - check + credit_note - debit_note + deposit + self.wire_transfer_amount
 
-		frappe.msgprint("balance en libros {} - {} + {} - {} + {} + {}".format(self.total_last_reconciliations,check,credit_note,debit_note,deposit,self.wire_transfer_amount))
-
+		frappe.msgprint("{} - {} + {} - {} + {} + {}".format(self.total_last_reconciliations, check, credit_note, debit_note, deposit, self.wire_transfer_amount))
 		filters_payments = self.filters_payment_amount_by_range()
 
 		payments = frappe.get_all("Payment Entry", ["*"], filters = filters_payments)
 
-		total_before = []
+		transit_transactions_arr = []
 
 		for payment in payments:
 			if datetime.strptime(str(payment.posting_date).split(" ")[0], '%Y-%m-%d') < datetime.strptime(str(self.from_date).split(" ")[0], '%Y-%m-%d'):
 				if payment.mode_of_payment == "Cheque":
 					book_balance += payment.paid_amount
-					total_before.append("cheque {}: {}".format(payment.name, payment.paid_amount))
-		
-		filters_payments = self.filters_payment_before()
-		
-		payments = frappe.get_all("Payment Entry", ["*"], filters = filters_payments)
-
-		for payment in payments:
-			if datetime.strptime(str(payment.posting_date).split(" ")[0], '%Y-%m-%d') < datetime.strptime(str(self.from_date).split(" ")[0], '%Y-%m-%d'):
-				# if payment.mode_of_payment == "Cheque":
-				# 	book_balance += payment.paid_amount
-				if payment.mode_of_payment == "Transferencia Bancaria":
-					book_balance -= payment.paid_amount
-					total_before.append("transferencia {}: {}".format(payment.name, payment.paid_amount))
+					transit_transactions_arr.append("cheque {}: {}".format(payment.name, payment.paid_amount))
 		
 		filters_transactions= self.filters_bank_transactions_amounts_by_range("check")
 		transactions = frappe.get_all("Bank Transactions", ["*"], filters = filters_transactions)
@@ -254,7 +240,7 @@ class BankBookReconciliations(Document):
 		for transaction in transactions:
 			if datetime.strptime(str(transaction.date_data).split(" ")[0], '%Y-%m-%d') < datetime.strptime(str(self.from_date).split(" ")[0], '%Y-%m-%d'):
 				book_balance += transaction.amount_data
-				total_before.append("cheque {}: {}".format(transaction.name, transaction.amount_data))
+				transit_transactions_arr.append("cheque {}: {}".format(transaction.name, transaction.amount_data))
 		
 		filters_transactions= self.filters_bank_transactions_amounts_by_range("debit")
 		transactions = frappe.get_all("Bank Transactions", ["*"], filters = filters_transactions)
@@ -262,7 +248,7 @@ class BankBookReconciliations(Document):
 		for transaction in transactions:
 			if datetime.strptime(str(transaction.date_data).split(" ")[0], '%Y-%m-%d') < datetime.strptime(str(self.from_date).split(" ")[0], '%Y-%m-%d'):
 				book_balance += transaction.amount_data
-				total_before.append("nota de debito {}: {}".format(transaction.name, transaction.amount_data))
+				transit_transactions_arr.append("nota de debito {}: {}".format(transaction.name, transaction.amount_data))
 		
 		filters_transactions= self.filters_bank_transactions_amounts_by_range("credit")
 		transactions = frappe.get_all("Bank Transactions", ["*"], filters = filters_transactions)
@@ -270,7 +256,7 @@ class BankBookReconciliations(Document):
 		for transaction in transactions:
 			if datetime.strptime(str(transaction.date_data).split(" ")[0], '%Y-%m-%d') < datetime.strptime(str(self.from_date).split(" ")[0], '%Y-%m-%d'):
 				book_balance -= transaction.amount_data
-				total_before.append("nota de credito {}: {}".format(transaction.name, transaction.amount_data))
+				transit_transactions_arr.append("nota de credito {}: {}".format(transaction.name, transaction.amount_data))
 		
 		filters_transactions= self.filters_bank_transactions_amounts_by_range("deposit")
 		transactions = frappe.get_all("Bank Transactions", ["*"], filters = filters_transactions)
@@ -278,9 +264,7 @@ class BankBookReconciliations(Document):
 		for transaction in transactions:
 			if datetime.strptime(str(transaction.date_data).split(" ")[0], '%Y-%m-%d') < datetime.strptime(str(self.from_date).split(" ")[0], '%Y-%m-%d'):
 				book_balance -= transaction.amount_data
-				total_before.append("deposito bancario {}: {}".format(transaction.name, transaction.amount_data))
-		
-		frappe.msgprint("{}".format(total_before))
+				transit_transactions_arr.append("deposito {}: {}".format(transaction.name, transaction.amount_data))
 
 		self.db_set('book_balance', book_balance, update_modified=False)
 
@@ -503,20 +487,7 @@ class BankBookReconciliations(Document):
 		conditions = ''
 
 		conditions += "{"
-		conditions += '"posting_date": ["<=", "{}"]'.format(self.to_date)
-		conditions += ', "prereconcilied": 1'
-		conditions += ', "reconciled": 0'
-		conditions += ', "company": "{}"'.format(self.company)
-		conditions += ', "bank_account": "{}"'.format(self.bank_account)
-		conditions += '}'
-
-		return conditions
-	
-	def filters_payment_before(self):
-		conditions = ''
-
-		conditions += "{"
-		conditions += '"posting_date": ["<=", "{}"]'.format(self.to_date)
+		conditions += '"posting_date": ["between", ["{}", "{}"]]'.format(self.from_date, self.to_date)
 		conditions += ', "prereconcilied": 1'
 		conditions += ', "reconciled": 0'
 		conditions += ', "company": "{}"'.format(self.company)
